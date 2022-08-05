ead2bf10fae15e3141390f1b9988fa1e0f98f154
Vinicius-SO <vhisoares39@gmail.com>
2022-08-05 10:28:16 -0300

    Component comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment(){
+    return(
+        <div className={styles.comment}>
+                    <img className={styles.avatar} src="https://github.com/Vinicius-SO.png" />
+                    <div className={styles.commentBox}>
+                        <div className={styles.commentContent}>
+                            <header>
+                                <div className={styles.authorAndTime}>
+                                    <strong>Vinicius Soares</strong>
+                                    <time title="08 Julho às 9:00" dateTime="2022-08-05 09:00:00">Cerca de 1h atrás</time>
+                                </div>
+
+                                <button title='Deleltar comentário'>
+                                    <Trash size={24}/>
+                                </button>
+                            </header>
+
+                            <p>Muito bom Devon, parabéns!! 👏👏</p>
+
+                            <footer>
+                                <button>
+                                    <ThumbsUp/>
+                                    Aplaudir <span>20</span>
+                                </button>
+                            </footer>
+                        </div>
+
+                    </div>
+
+        </div>
+    )
+}
