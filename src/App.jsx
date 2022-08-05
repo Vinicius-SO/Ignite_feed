d889ea1b554790eb63f0aca42c46c4a1cd417fa2
Vinicius-SO <vhisoares39@gmail.com>
2022-08-04 22:07:50 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Post } from "./Post"
+
+function App() {
+  return (
+    <>
+      <Post author="Vinicius" description="Propriedades No React"/>
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/Post.jsx
@@ -0,0 +1,9 @@
+export function Post(props){
+    return(
+        <>
+            <p>Post</p>
+            <p>{props.author}</p>
+            <p>{props.description}</p>
+        </>
+    )
+}
