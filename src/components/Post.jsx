508188c48792b377bb7fafaad1b0a63711ce01e4
Vinicius-SO <vhisoares39@gmail.com>
2022-08-05 22:55:05 -0300

    Content iteration

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,7 +6,7 @@ import { Avatar } from "./Avatar"
 import styles from "./Post.module.css"
 
 
-export function Post({author, publishedAt}){
+export function Post({author, publishedAt, content}){
     const publisheadAtFormated = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {locale:ptBr})
     
     const publishedDateRelativeToNow = formatDistanceToNow(publishedAt,{
@@ -32,14 +32,13 @@ export function Post({author, publishedAt}){
             </header>
 
             <div className={styles.content}>
-                <p>Fala galeraa{' '}👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
-                <p>
-                    <a href="">#novoprojeto</a>{' '}
-                    <a href="">#nlw</a>{' '}
-                    <a >#rockeatseat</a>    
-                </p>
+                {content.map(line => {
+                    if (line.type === 'paragraph') {
+                        return <p>{line.content}</p>;
+                    } else if (line.type === 'link') {
+                        return <p><a href="#">{line.content}</a></p>
+                    }
+                })}
             </div>
 
             <form className={styles.commentForm}>
