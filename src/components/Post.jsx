0a82dea87db3f99f29f7cad3569214e64f181b42
Vinicius-SO <vhisoares39@gmail.com>
2022-08-08 15:41:29 -0300

    Comments iteration

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,11 +4,28 @@ import ptBr from 'date-fns/locale/pt-BR';
 import { Comment } from './Comment.jsx'
 import { Avatar } from "./Avatar"
 import styles from "./Post.module.css"
+import { useState } from 'react';
 
 
 export function Post({author, publishedAt, content}){
     const publisheadAtFormated = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {locale:ptBr})
     
+
+    const [comments, setComments] = useState([
+        1,
+        2,
+        3
+    ])
+
+
+    function handleCreateNewComment (){
+        event.preventDefault()
+        console.log('handle')
+        setComments([...comments, comments.length+1])
+        console.log(comments)
+    }
+    
+
     const publishedDateRelativeToNow = formatDistanceToNow(publishedAt,{
         locale: ptBr,
         addSuffix: true
@@ -41,7 +58,7 @@ export function Post({author, publishedAt, content}){
                 })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu comentario</strong>
                 
                 <textarea placeholder="Deixe seu comentario"/>
@@ -52,9 +69,10 @@ export function Post({author, publishedAt, content}){
             </form>
 
             <div className={styles.commentList}>
-                <Comment/>
-                <Comment/>
-                <Comment/>
+                {comments.map(()=>(
+                    <Comment></Comment>
+                )
+                )}
             </div>
         </article>
     )
